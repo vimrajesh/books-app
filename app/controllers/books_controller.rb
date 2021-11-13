75bf789b840b61cf922a1e063c8c0a662b8b287b
ARJUN <arjundps2000@gmail.com>
2021-11-13 21:35:10 +0530

    full functionality
    Co-authored-by: Vimal Rajesh <vimrajesh@users.noreply.github.com>

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,13 +3,13 @@ require 'json'
 class BooksController < ApplicationController
   def index
     @books = Book.all
-    render json: @books.as_json(only: [:B_id, :B_title, :Author, :Publisher, :Year] )
+    # render json: @books.as_json(only: [:B_id, :B_title, :Author, :Publisher, :Year] )
   end
 
   def show
     @book = Book.find(params[:id])
-    @answer = JSON.pretty_generate(@book.as_json(only: [:B_id, :B_title, :Author, :Publisher, :Year] ))
-    render json: @answer
+    # @answer = JSON.pretty_generate(@book.as_json(only: [:B_id, :B_title, :Author, :Publisher, :Year] ))
+    # render json: @answer
   end
 
   def new
