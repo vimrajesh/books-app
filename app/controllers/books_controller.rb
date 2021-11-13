620990b1b4620bd343d5262943c7f8d5f35ff11e
ARJUN <arjundps2000@gmail.com>
2021-11-14 00:57:46 +0530

    all functionality done
    Co-authored-by: Vimal Rajesh <vimrajesh@users.noreply.github.com>

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,42 @@
 require 'json'
 
 class BooksController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def home
+    @books = Book.all
+    render json: @books.as_json(only: [:B_id, :B_title, :Author, :Publisher, :Year]), status: :ok
+  end
+
+  def homeRaw
+    @books = Book.all
+    render json: @books, status: :ok
+  end
+
   def index
     @books = Book.all
-    # render json: @books.as_json(only: [:B_id, :B_title, :Author, :Publisher, :Year] )
   end
 
   def show
     @book = Book.find(params[:id])
-    # @answer = JSON.pretty_generate(@book.as_json(only: [:B_id, :B_title, :Author, :Publisher, :Year] ))
-    # render json: @answer
+  end
+
+  def display
+    begin
+      @book = Book.find(params[:id])
+      render json: @book.as_json(only: [:B_id, :B_title, :Author, :Publisher, :Year]), status: :ok
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { error: 'Book not found' } , status: :not_found
+    end
+  end
+  
+  def displayRaw
+    begin
+      @book = Book.find(params[:id])
+      render json: @book, status: :ok
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { error: 'Book not found' } , status: :not_found
+    end
   end
 
   def new
@@ -18,13 +45,25 @@ class BooksController < ApplicationController
 
   def create
     book = Book.create(book_params)
-
     redirect_to books_path
   end
 
   def edit
     @book = Book.find(params[:id])
   end
+
+  def addBook
+    @book = Book.new(book_params)
+    
+    puts @book 
+
+    if @book.save 
+        render json: {status: 'SUCCESS' , book: @book} , status: :ok
+    else
+        render json: {status: 'FAILED' , error:@book.errors} , status: :bad_request
+    end
+  end
+
 
   def update
     @book = Book.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get '/books/fetch_books' , to: 'books#home'
+  get '/books/fetch_books_raw' , to: 'books#homeRaw'
+  get '/books/book/:id' , to: 'books#display'
+  get '/books/book/raw/:id' , to: 'books#displayRaw'
   resources :books
+  post '/books/add_book'  , to: 'books#addBook'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
